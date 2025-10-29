a2b005ed3f6516c13907a4201065ef1a9ede3af4
Carsen Cote <cotec2@myumanitoba.ca>
2025-10-29 08:36:28 -0500

    Added templates for sign in page and blackhole page, working on transition.

---
+++ src/main.js
@@ -0,0 +1,29 @@
+var userFirstName;
+var userLastName;
+var userEmail;
+
+$(function() {
+    $('#main').append($('#signin-template').html());
+});
+
+$(function() {
+    $('#signin-form').on('submit', function(event) {
+        event.preventDefault();
+        var dataString = $(this).serialize();
+        var params = new URLSearchParams(dataString);
+        const fname = params.get('fname');
+        const lname = params.get('lname');
+        const email = params.get('email');
+        userFirstName = fname;
+        userLastName = lname;
+        userEmail = email;
+        changeMainTemplate('blackhole-template');
+    })
+});
+
+function changeMainTemplate(templateId) {
+    $('#main').empty().append($('#' + templateId).html())
+}
+
+
+
