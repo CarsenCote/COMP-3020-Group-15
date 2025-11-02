385c25c8d85145aef1503af070538d1a115a4a01
Carsen Cote <cotec2@myumanitoba.ca>
2025-11-02 08:46:20 -0600

    Changed some styles and added a better structure for the main javascript file for the project using OOP (It's not that bad I promise).

--- src/main.js
+++ src/main.js
@@ -1,53 +1,120 @@
-const BLACKHOLE_TEMPLATE = 'blackhole-template';
-const CATEGORIES_TEMPLATE = 'categories-template';
+// Template classes
 
-var userFirstName;
-var userLastName;
-var userEmail;
+class Template {
 
-// This function is executed when the interface is first opened and the document is ready.
-$(function () {
-    $('#main').append($('#signin-template').html());
-});
+    templateId;
+    
+    constructor(templateId) {
+        this.templateId = templateId;
+    }
 
-// This is a callback function that is called when the form on the sign-in page is submitted.
-$(function () {
-    $('#signin-form').on('submit', function (event) {
-        event.preventDefault();
-        var dataString = $(this).serialize();
-        var params = new URLSearchParams(dataString);
-        const fname = params.get('fname');
-        const lname = params.get('lname');
-        const email = params.get('email');
-        userFirstName = fname;
-        userLastName = lname;
-        userEmail = email;
-        changeMainTemplate(BLACKHOLE_TEMPLATE);
-    })
-});
+    setup () {
+        this.setupEventListeners();
+        this.setupElements();
+    }
 
-function changeMainTemplate(templateId) {
-    $('#main').empty().append($('#' + templateId).html());
+    setupEventListeners () {
+        
+    }
 
-    // Set up event listeners based on which template was loaded
-    switch (templateId) {
-        case BLACKHOLE_TEMPLATE:
-            setupBlackholeEvents();
-            break;
-        case CATEGORIES_TEMPLATE:
-            setupCategoriesEvents();
-            break;
+    setupElements () {
+
     }
 }
 
-function setupBlackholeEvents() {
-    $('.blackhole-container').on('click', function () {
-        changeMainTemplate(CATEGORIES_TEMPLATE);
-    });
+class BlackHoleTemplate extends Template {
+
+    constructor(templateId) {
+        super(templateId);
+    }
+
+    setupEventListeners() {
+        
+        $('.blackhole-container').on('click', function () {
+            changeMainTemplate(CATEGORIES_TEMPLATE);
+        });
+    }
+
+    setupElements() {
+
+    }
 }
 
-function setupCategoriesEvents() {
-    $('.categories-container')
+class SignInTemplate extends Template {
+
+    constructor(templateId) {
+        super(templateId);
+    }
+    
+    setupEventListeners() {
+        $('#signin-form').on('submit', function (event) {
+            event.preventDefault();
+            var dataString = $(this).serialize();
+            var params = new URLSearchParams(dataString);
+            const fname = params.get('fname');
+            const lname = params.get('lname');
+            const email = params.get('email');
+            user = new User(fname, lname, email);
+            changeMainTemplate(BLACKHOLE_TEMPLATE);
+        })
+    }
+
+    setupElements() {
+
+    }
+}
+
+class CategoriesTemplate extends Template {
+
+    constructor(templateId) {
+        super(templateId);
+    }
+
+    setupEventListeners() {
+        
+    }
+
+    setupElements() {
+
+    }
+}
+
+// General classes
+
+class User {
+    firstName;
+    lastName;
+    email;
+
+    constructor(firstName, lastName, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
+
+
+
+/*
+This is the main section of code execution after classes have been declared.
+*/
+
+const BLACKHOLE_TEMPLATE = new BlackHoleTemplate('blackhole-template');
+const CATEGORIES_TEMPLATE = new CategoriesTemplate('categories-template');
+const SIGN_IN_TEMPLATE = new SignInTemplate('signin-template');
+
+var currentPage;
+var user;
+
+// This function is executed when the interface is first opened and the document is ready.
+$(function () {
+    changeMainTemplate(SIGN_IN_TEMPLATE);
+});
+
+function changeMainTemplate(template) {
+    currentPage = template;
+    $('#main').empty().append($('#' + template.templateId).html());
+    currentPage.setup();
 }
 
 
